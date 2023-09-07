5ee0e887feeb983f100297f9c67a247b784f17d2
LizMiranda00 <102626073+LizMiranda00@users.noreply.github.com>
2023-09-07 00:12:05 -0600

    DEVUELVE

--- src/Interfaces/Prestamo/Show.js
+++ src/Interfaces/Prestamo/Show.js
@@ -4,12 +4,57 @@ import home from '../../img/home.png';
 import categ from '../../img/1164620.png';
 import '../Estilos.css';
 import axios from 'axios';
+import Modal from 'react-modal';
 
 const iconos = [{ src: home, alt: 'home' }, { src: categ, alt: 'clientes' }];
 
 const Show = () => {
   const [prestamos, setPrestamos] = useState([]);
+  
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedPrestamoId, setSelectedPrestamoId] = useState(null);
+  const [selectedLibros, setSelectedLibros] = useState([]);
+ 
+  const [observaciones, setObservaciones] = useState(''); 
+  const openModal = (prestamoId) => {
+    setSelectedPrestamoId(prestamoId);
+    setModalOpen(true);
+  };
+  
+  const closeModal = () => {
+    setSelectedPrestamoId(null);
+    setSelectedLibros([]);
+    setModalOpen(false);
+  };
+  
+  const handleCheckboxChange = (libro) => {
+    setSelectedLibros(prev => {
+      if (prev.includes(libro)) {
+        return prev.filter(item => item !== libro);
+      } else {
+        return [...prev, libro];
+      }
+    });
+  };
+  
+  const handleDevolverLibros = async () => {
+    try {
+      // Aquí puedes enviar los libros seleccionados al servidor
+      const response = await axios.post('http://192.168.100.5/bliblioteca/public/api/devolucion', {
+        prestamo_id: selectedPrestamoId,
+        libros: selectedLibros,
+        observaciones: observaciones, 
+      });
 
+      // Lógica para manejar la respuesta de la API
+      console.log(response.data);
+
+      // Finalmente, cierra el modal
+      closeModal();
+    } catch (error) {
+      console.error('Error al enviar los datos:', error);
+    }
+  };
   useEffect(() => {
     // Realizar una solicitud GET para obtener la lista de préstamos desde tu API
     axios.get('http://192.168.100.5/bliblioteca/public/api/prestamos')
@@ -39,10 +84,42 @@ const Show = () => {
             <p>Fecha de Devolución: {prestamo.fecha_devolucion}</p>
             <p>Cantidad: {prestamo.cantidad}</p>
             <p>Libros: {prestamo.libros.join(', ')}</p> {/* Mostrar los nombres de los libros separados por comas */}
-           <button>Devolver</button>
+           <button onClick={() => openModal(prestamo.id)}>Devolver</button>
           </div>
         ))}
-      </div>
+ </div>
+<Modal
+        className='react-modal-content'
+        isOpen={modalOpen}
+        onRequestClose={closeModal}
+        contentLabel='Devolver Préstamo'
+      >
+        <h1>Devolver Préstamo ID: {selectedPrestamoId}</h1>
+        {prestamos
+          .find(prestamo => prestamo.id === selectedPrestamoId)
+          .libros.map((libro, index) => (
+            <label key={index}>
+              <input
+                type="checkbox"
+                value={libro}
+                checked={selectedLibros.includes(libro)}
+                onChange={() => handleCheckboxChange(libro)}
+              />
+              {libro}
+            </label>
+          ))}
+           <label className='letras'>Observaciones:</label>
+        <textarea
+          value={observaciones}
+          onChange={(e) => setObservaciones(e.target.value)}
+        />
+        <button onClick={handleDevolverLibros}>Devolver Libros</button>
+        <button onClick={closeModal}>Cancelar</button>
+      </Modal>
+
+
+
+     
     </div>
   );
 };
