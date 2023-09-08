dd44e58aef52682900d71f6d314a0ded268f4486
darwing-21 <darwingarcia2130@gmail.com>
2023-09-08 11:42:34 -0400

    prueba login

--- src/App.js
+++ src/App.js
@@ -1,65 +1,56 @@
-import React ,{useState}from 'react'
+import React, { useState } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Principal from './Interfaces/Principal';
-import Admi from './Interfaces/Admi';
-import Empleado from './Interfaces/Empleado'
-import Reglib from './Interfaces/Reglib';
-import Regper from './Interfaces/Regper';
-import AutorShow from './Interfaces/Autor/Show';
-import AutorCreate from './Interfaces/Autor/Create';
-import AutorDelete from './Interfaces/Autor/Delete';
-import CategoriaShow from './Interfaces/Categoria/Show'
-import CategoriaCreate from './Interfaces/Categoria/Create'
-import LibroShow from './Interfaces/Libro/Show'
-import LibroCreate from './Interfaces/Libro/Create'
-import Login from './Login'
-import ClienteCreate from './Interfaces/Cliente/Create';
-import ClienteShow from './Interfaces/Cliente/Show';
-import PrestamoAlquiler from './Interfaces/Prestamo/Alquiler';
-import PrestamoShow from './Interfaces/Prestamo/Show'
+import Principal from "./Interfaces/Principal";
+import Admi from "./Interfaces/Admi";
+import Empleado from "./Interfaces/Empleado";
+import Reglib from "./Interfaces/Reglib";
+import Regper from "./Interfaces/Regper";
+import AutorShow from "./Interfaces/Autor/Show";
+import AutorCreate from "./Interfaces/Autor/Create";
+import AutorDelete from "./Interfaces/Autor/Delete";
+import CategoriaShow from "./Interfaces/Categoria/Show";
+import CategoriaCreate from "./Interfaces/Categoria/Create";
+import LibroShow from "./Interfaces/Libro/Show";
+import LibroCreate from "./Interfaces/Libro/Create";
+import Login from "./Login";
+import ClienteCreate from "./Interfaces/Cliente/Create";
+import ClienteShow from "./Interfaces/Cliente/Show";
+import PrestamoAlquiler from "./Interfaces/Prestamo/Alquiler";
+import PrestamoShow from "./Interfaces/Prestamo/Show";
 const App = () => {
   const [user, setUser] = useState("");
   return (
-    
-      <BrowserRouter>
+    <BrowserRouter>
       <Routes>
-        <Route
-          path="/login"
-          element={user ? <Navigate to="/Libro" /> : <Login setUser={setUser} />}
-        />
-        <Route
-          path="/"
-          element={user ? <Principal /> : <Navigate to="/login" />}
-        />
-        <Route path='/' element={<Principal/>}></Route>
-        
-        <Route path='/Adm' element={<Admi/>}></Route>
-        <Route path='/Emp' element={<Empleado/>}></Route>
-        
-        <Route path='/Regli' element={<Reglib/>}></Route>
-        <Route path='/Regpe' element={<Regper/>}></Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Principal />} />
+        <Route path="/" element={<Principal />}></Route>
 
-        <Route path='/Autor' element={<AutorShow/>}></Route>
-        <Route path='/Autor/Create' element={<AutorCreate/>}></Route>
-        <Route path='/Autor/Delete' element={<AutorDelete/>}></Route>
+        <Route path="/Adm" element={<Admi />}></Route>
+        <Route path="/Emp" element={<Empleado />}></Route>
 
-        <Route path='/Categoria' element={<CategoriaShow/>}></Route>
-        <Route path='/Categoria/Create' element={<CategoriaCreate/>}></Route>
-        
-        <Route path='/Libro' element={<LibroShow/>}></Route>
-        <Route path='/Libro/Create' element={<LibroCreate/>}></Route>
-        <Route path='/Login' element={<Login/>}></Route>
-        
-        <Route path='/Cliente/Create' element={<ClienteCreate/>}></Route>
-        <Route path='/Cliente' element={<ClienteShow/>}></Route>
+        <Route path="/Regli" element={<Reglib />}></Route>
+        <Route path="/Regpe" element={<Regper />}></Route>
 
-        <Route path='/Prestamo/Alquiler' element={<PrestamoAlquiler/>}></Route>
-        <Route path='/Prestamo/Show' element={<PrestamoShow/>}></Route>
+        <Route path="/Autor" element={<AutorShow />}></Route>
+        <Route path="/Autor/Create" element={<AutorCreate />}></Route>
+        <Route path="/Autor/Delete" element={<AutorDelete />}></Route>
+
+        <Route path="/Categoria" element={<CategoriaShow />}></Route>
+        <Route path="/Categoria/Create" element={<CategoriaCreate />}></Route>
+
+        <Route path="/Libro" element={<LibroShow />}></Route>
+        <Route path="/Libro/Create" element={<LibroCreate />}></Route>
+        <Route path="/Login" element={<Login />}></Route>
+
+        <Route path="/Cliente/Create" element={<ClienteCreate />}></Route>
+        <Route path="/Cliente" element={<ClienteShow />}></Route>
+
+        <Route path="/Prestamo/Alquiler" element={<PrestamoAlquiler />}></Route>
+        <Route path="/Prestamo/Show" element={<PrestamoShow />}></Route>
       </Routes>
-      </BrowserRouter>
+    </BrowserRouter>
+  );
+};
 
-   
-  )
-}
-
-export default App
+export default App;
--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,12 @@
 import axios from "axios";
-import {ipAddress} from "./Componentes/confip";
+import { ipAddress } from "./Componentes/confip";
 import React, { useState } from "react";
-import logo from "./img/logo.png"
+import logo from "./img/logo.png";
+import { useNavigate } from "react-router-dom";
 
-function Login({setUser}) {
+
+function Login({ setUser }) {
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPasword] = useState("");
   const [error, setError] = useState(false);
@@ -17,7 +20,6 @@ function Login({setUser}) {
     }
     setError(false);
 
-
     try {
       const response = await axios.post(
         `http://${ipAddress}/app/bliblioteca/public/api/login`,
@@ -27,53 +29,64 @@ function Login({setUser}) {
         }
       );
 
-     
       console.log("Respuesta del servidor:", response.data);
 
-      // Ejemplo de cómo establecer el usuario en el estado si la autenticación es exitosa
-      
-    setUser([email]);
+      if (response.data.rol === "admin") {
+       
+        navigate("/Libro");
+      } else if (response.data.rol === "user") {
 
+        navigate("/Autor");
+      }
     } catch (error) {
       // Manejo de errores en caso de una respuesta de error del servidor
       console.error("Error al iniciar sesión:", error);
     }
   };
   return (
-    <div class="container d-flex justify-content-center align-items-center vh-100" style={{backgroundImage: `url(${logo})`,backgroundSize: 'cover', backgroundPosition: 'center'}}>
-              <div class="card">
-                  <h1 class="card-title text-center align-items-center" >Login </h1>
-                  <div class="card-body">
-                    <form action="" onSubmit={handleSubmit}>
-                      <div class="row">
-                        <p class="card-text">E-mail</p>
-                        <input
-                            class="form-control"
-                            type="email"
-                            name="email"
-                            value={email}
-                            onChange={e => setEmail(e.target.value)}
-                          />
-                      </div>
-                      <div class="row">
-                        <p class="card-text">Contraseña:</p>
-                        <input
-                          class="form-control"
-                          type="password"
-                          name="password"
-                          value={password}
-                          onChange={e => setPasword(e.target.value)}
-                        />
-                      </div>
-                      
-                        <div class="row mt-3">
-                          	<button type="submit" class="btn btn-primary">Login</button>
-                        </div>
-                    </form>
-                    {error && <p>Todos los campos son obligatorios</p>}
-                  </div>
-              </div>
+    <div
+      class="container d-flex justify-content-center align-items-center vh-100"
+      style={{
+        backgroundImage: `url(${logo})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div class="card">
+        <h1 class="card-title text-center align-items-center">Login </h1>
+        <div class="card-body">
+          <form action="" onSubmit={handleSubmit}>
+            <div class="row">
+              <p class="card-text">E-mail</p>
+              <input
+                class="form-control"
+                type="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div class="row">
+              <p class="card-text">Contraseña:</p>
+              <input
+                class="form-control"
+                type="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPasword(e.target.value)}
+              />
+            </div>
+
+            <div class="row mt-3">
+              <button type="submit" class="btn btn-primary">
+                Login
+              </button>
+            </div>
+          </form>
+          {error && <p>Todos los campos son obligatorios</p>}
+        </div>
       </div>
+    </div>
   );
 }
 
